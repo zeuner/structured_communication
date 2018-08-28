a31f3ed28652727961fc94836c3bf4a0fb5be841
Isidor Zeuner <minetest@quidecco.de>
2018-08-28 19:05:14 +0200

    initial import

---
+++ init.lua
@@ -0,0 +1,34 @@
+local S
+
+if minetest.get_modpath(
+    "intllib"
+) then
+    S = intllib.Getter(
+    )
+else
+    S = function(
+        translated
+    )
+        return translated
+    end
+end
+
+minetest.register_on_rightclickplayer(
+    function(
+        clicked,
+        clicker
+    )
+        minetest.show_formspec(
+            clicked:get_player_name(
+            ),
+            "structured_communication",
+            "size[7,7]"
+        )
+        minetest.show_formspec(
+            clicker:get_player_name(
+            ),
+            "structured_communication",
+            "size[7,7]"
+        )
+    end
+)
