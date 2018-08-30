385a9bd54fd69ecdd2ba2cd2f9e0d31b8827eb14
Isidor Zeuner <minetest@quidecco.de>
2018-08-30 12:32:26 +0200

    synchronously open and close formspec

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+local die
+
+local fatal = function(
+    message
+)
+    print(
+        "FATAL: " .. message
+    )
+    die(
+    )
+end
+
 local S
 
 if minetest.get_modpath(
@@ -13,22 +25,90 @@ else
     end
 end
 
+local sessions = {
+}
+
 minetest.register_on_rightclickplayer(
     function(
         clicked,
         clicker
     )
+        local clicked_name = clicked:get_player_name(
+        )
+        local clicker_name = clicker:get_player_name(
+        )
+        if sessions[
+            clicker_name
+        ] then
+            fatal(
+                "right-clicking players should not be possible right now"
+            )
+        end
+        if sessions[
+            clicked_name
+        ] then
+            minetest.chat_send_player(
+                clicker_name,
+                string.format(
+                    S(
+                        "player %s is busy"
+                    ),
+                    clicked_name
+                )
+            )
+            return
+        end
+        sessions[
+            clicked_name
+        ] = clicker_name
+        sessions[
+            clicker_name
+        ] = clicked_name
         minetest.show_formspec(
-            clicked:get_player_name(
-            ),
+            clicked_name,
             "structured_communication:main",
             "size[7,7]"
         )
         minetest.show_formspec(
-            clicker:get_player_name(
-            ),
+            clicker_name,
             "structured_communication:main",
             "size[7,7]"
         )
     end
 )
+
+minetest.register_on_player_receive_fields(
+    function(
+        player,
+        formname,
+        fields
+    )
+        if "structured_communication:main" ~= formname then
+            return false
+        end
+        local name = player:get_player_name(
+        )
+        local other = sessions[
+            name
+        ]
+        if not other then
+            fatal(
+                "no other player registered"
+            )
+        end
+        if not fields.quit then
+            return true
+        end
+        minetest.close_formspec(
+            other,
+            "structured_communication:main"
+        )
+        sessions[
+            other
+        ] = nil
+        sessions[
+            name
+        ] = nil
+    end
+)
+
